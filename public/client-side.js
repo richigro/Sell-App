f479e4b3b1f26e70d334c66934a286474f20210d
richigro <richi_gro@hotmail.com>
2018-11-06 19:18:32 -0600

    new js file with mock data and included jquery cdn

---
+++ public/client-side.js
@@ -0,0 +1,38 @@
+const MOCK_ITEMS_ON_SALE = {
+    "itemsOnSale": [
+        {
+            "id": "1111111",
+            "title": "Sewing Machine",
+            "price": "100",
+            "description": "John Doe",
+            "contact": {
+                "phone": "+210123578",
+                "email": "asdf@asdf.com"
+            }, 
+            "publishedAt": 1470016976609
+        }, 
+        {
+            "id": "1111111",
+            "title": "Sewing Machine",
+            "price": "100",
+            "description": "John Doe",
+            "contact": {
+                "phone": "+210123578",
+                "email": "asdf@asdf.com"
+            }, 
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "1111111",
+            "title": "Sewing Machine",
+            "price": "100",
+            "description": "John Doe",
+            "contact": {
+                "phone": "+210123578",
+                "email": "asdf@asdf.com"
+            }, 
+            "publishedAt": 1470016976609
+        }
+    ]
+};
+
