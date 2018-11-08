23e15c10beceef0c48b35ca13000673c2914d826
Ricardo Guerrero <richi_gro@hotmail.com>
2018-11-08 16:23:51 -0600

    added new function to load in the product detail page

--- public/client-side.js
+++ public/client-side.js
@@ -54,6 +54,26 @@ function homePage(data){
 
         </footer>`;
 }
+
+function productDetailPage(){
+    return `
+        <main role="main">
+            <h1>Item title here</h1>
+            <img src="#"/>
+            <div class="item-description">
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            </div>
+            <div class="seller-info">
+                <h1>Contact Seller</h1>
+                <p>email: dfaf@adaf.com</p>
+                <p>phone: 21341231234</p>
+                <p>Location Houston Texas</p>
+            </div>
+        </main>
+    `;
+}
+
+
 function loginOrSignUp(){
     $(".js-login-page").on("click", ".js-app-container", (event) => {
         console.log("login button clicked!");
