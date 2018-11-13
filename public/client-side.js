6e765287e1070c760aa0822c3a10d33500685326
Ricardo Guerrero <richi_gro@hotmail.com>
2018-11-12 20:49:42 -0600

    product page matches selected element to apropriate item's description

--- public/client-side.js
+++ public/client-side.js
@@ -65,16 +65,16 @@ function itemList(item){
 function productDetailPage(item){
     return `
         <main role="main">
-            <h1>Item title here</h1>
+            <h1>${item.title}</h1>
             <img src="#"/>
             <div class="item-description">
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                <p>${item.description}</p>
             </div>
             <div class="seller-info">
                 <h1>Contact Seller</h1>
-                <p>email: dfaf@adaf.com</p>
-                <p>phone: 21341231234</p>
-                <p>Location Houston Texas</p>
+                <p>email: ${item.contact["email"]}</p>
+                <p>phone: ${item.contact["phone"]}</p>
+                <p>Location:</p>
             </div>
         </main>
     `;
