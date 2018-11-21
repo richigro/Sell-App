185bda814104b0f265f9ba55c10821b0882fcf1e
richigro <richi_gro@hotmail.com>
2018-11-20 22:41:34 -0600

    creating item schem and model

--- models.js
+++ models.js
@@ -2,3 +2,19 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
+const itemSchema = mongoose.Schema({
+    image: {type: String, required: true},
+    name: {type: String, required: true},
+    price: {type: String, required: true},
+    description: {type: String, required: true},
+    'short-description': {type: String, required: true},
+    contact: {
+        seller: {type: String, required: true},
+        phone: {type: String, required: true},
+        email: {type: String, required: true},
+        location: {type: String, required: true}
+    },
+    publishedAt: Number
+});
+
+module.exports = { Item };
