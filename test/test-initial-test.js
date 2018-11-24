985a633fdb1540c2fe9bdecbb7c1cc1a7c04fb80
Ricardo Guerrero <richi_gro@hotmail.com>
2018-11-24 15:04:49 -0600

    completed integration test for GET on /for-sale endpoint

--- test/test-initial-test.js
+++ test/test-initial-test.js
@@ -9,18 +9,27 @@ chai.use(chaiHttp);
 
 describe('Simple Test', function() {
     before(function() {
-        return runServer();
+        return runServer('mongodb://localhost/sell-app');
     });
     // close server after testing
     after(function() {
         return closeServer();
     });
-
-    it("Should get 200 server satus", function() {
+    // testing get endpoint for sale items
+    it("Should get all items for sale on db", function() {
         return chai.request(app)
-        .get("/")
+        .get("/for-sale")
         .then(function(res) {
             expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body.items).to.be.a('array');
+            expect(res.body.items.length).to.be.above(0);
+            res.body.items.forEach(function(item) {
+                expect(item).to.be.a('object');
+                expect(item).to.have.all.keys('_id','name', 'contact', 'description', 'short-description', 'image', 'price', 'publishedAt');
+            });
+
         });
     });
+    //testing post item for sale
 });
