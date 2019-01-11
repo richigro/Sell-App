10a9d82cc441bdeaac5f84c870bb99dfa84d6553
Ricardo Guerrero <richi_gro@hotmail.com>
2019-01-10 21:27:34 -0600

    good

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require('body-parser');
 // const jsonParser = bodyParser.json();
+const passport = require('passport');
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
@@ -15,6 +18,38 @@ const {User} = require('./models');
 app.use(morgan('common'));
 app.use(express.static('public'));
 // app.use(jsonParser);
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
+
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
+
 //fix post bug
 app.use(bodyParser.urlencoded({
   extended: true
