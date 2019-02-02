8df9af1ec4a3949e70c6b75b265b09aa2164f407
Ricardo Guerrero <richi_gro@hotmail.com>
2019-02-01 20:15:52 -0600

    fixed minor bug in testing

---
+++ test/test-Users.js
@@ -0,0 +1,16 @@
+// implementation pending
+// 'use strict';
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// //generates fake data for testing
+// const faker = require('faker');
+
+// const { TEST_DATABASE_URL } = require('../config');
+// const { USERS_URL } = require('../config');
+// const { app, runServer, closeServer} = require('../server');
+// const {User} = require('../users/user-model');
+// const expect = chai.expect;
+
+// chai.use(chaiHttp);
